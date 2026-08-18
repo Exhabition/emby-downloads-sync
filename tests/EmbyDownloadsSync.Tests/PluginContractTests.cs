5840f83132bb1934fe68fc7cc94b16fec4fc6ace
Exhabition <lukeweerheim@hotmail.com>
2026-08-18 16:08:58 +0200

    feat: simplify settings experience

--- tests/EmbyDownloadsSync.Tests/PluginContractTests.cs
+++ tests/EmbyDownloadsSync.Tests/PluginContractTests.cs
@@ -1,13 +1,16 @@
 using System.Reflection.Metadata;
 using System.Reflection.PortableExecutable;
 using EmbyDownloadsSync.Plugin.Api;
+using EmbyDownloadsSync.Plugin.Configuration;
 using EmbyDownloadsSync.Plugin.Integration;
 using EmbyDownloadsSync.Plugin.Tasks;
+using EmbyDownloadsSync.Plugin.UI;
 using MediaBrowser.Common.Plugins;
 using MediaBrowser.Controller.Net;
 using MediaBrowser.Controller.Plugins;
 using MediaBrowser.Model.Plugins;
 using MediaBrowser.Model.Plugins.UI;
+using MediaBrowser.Model.Attributes;
 using MediaBrowser.Model.Services;
 using MediaBrowser.Model.Tasks;
 
@@ -43,6 +46,34 @@ public sealed class PluginContractTests
     }
 
     [Fact]
+    public void RouteEditorKeepsTheEverydayWorkflowSimple()
+    {
+        AssertSimple<DashboardPageOptions>(
+            nameof(DashboardPageOptions.RouteName),
+            nameof(DashboardPageOptions.RouteEnabled),
+            nameof(DashboardPageOptions.SourceDeviceIds),
+            nameof(DashboardPageOptions.TargetDeviceIds));
+        AssertAdvanced<DashboardPageOptions>(
+            nameof(DashboardPageOptions.RouteId),
+            nameof(DashboardPageOptions.Topology),
+            nameof(DashboardPageOptions.MatchMode),
+            nameof(DashboardPageOptions.IncludeNamePattern),
+            nameof(DashboardPageOptions.OverrideQuality),
+            nameof(DashboardPageOptions.MaximumCreates));
+    }
+
+    [Fact]
+    public void GlobalTuningOptionsAreAdvanced()
+    {
+        AssertSimple<PluginOptions>(nameof(PluginOptions.Enabled), nameof(PluginOptions.ApiKey), nameof(PluginOptions.DryRun));
+        AssertAdvanced<PluginOptions>(
+            nameof(PluginOptions.AllowManagedCleanup),
+            nameof(PluginOptions.DefaultMaximumCreates),
+            nameof(PluginOptions.RetainedRunCount),
+            nameof(PluginOptions.HttpTimeoutSeconds));
+    }
+
+    [Fact]
     public void EveryApiRequestRequiresAdministratorAuthentication()
     {
         var requestTypes = typeof(GetSyncRoutes).Assembly.GetExportedTypes()
@@ -71,4 +102,10 @@ public sealed class PluginContractTests
         var root = Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, "../../../../.."));
         return Path.Combine(root, "src", "EmbyDownloadsSync.Plugin", "bin", configuration, "netstandard2.0", "deploy", "EmbyDownloadsSync.dll");
     }
+
+    private static void AssertAdvanced<T>(params string[] propertyNames) => Assert.All(propertyNames, propertyName =>
+        Assert.NotNull(typeof(T).GetProperty(propertyName)!.GetCustomAttributes(typeof(IsAdvancedAttribute), true).SingleOrDefault()));
+
+    private static void AssertSimple<T>(params string[] propertyNames) => Assert.All(propertyNames, propertyName =>
+        Assert.Empty(typeof(T).GetProperty(propertyName)!.GetCustomAttributes(typeof(IsAdvancedAttribute), true)));
 }
